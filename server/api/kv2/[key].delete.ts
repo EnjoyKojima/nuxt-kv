e7f745c05afd20b98c1a253409aab5771b1aca59
EnjoyKojima <majiko2627kojima@gmail.com>
2025-05-09 23:27:37 +0900

    deleteにbodyを使うとエラーになる

--- server/api/kv2/[key].delete.ts
+++ server/api/kv2/[key].delete.ts
@@ -2,7 +2,7 @@ import { zh } from 'h3-zod'
 import { z } from 'zod'
 
 export default defineEventHandler(async (event) => {
-  const { key } = await zh.useValidatedBody(event, z.object({
+  const { key } = await zh.useValidatedParams(event, z.object({
     key: z.string(),
   }))
 
--- server/api/kv2/[key].put.ts
+++ server/api/kv2/[key].put.ts
@@ -2,8 +2,10 @@ import { zh } from 'h3-zod'
 import { z } from 'zod'
 
 export default defineEventHandler(async (event) => {
-  const { key, value } = await zh.useValidatedBody(event, z.object({
+  const { key } = await zh.useValidatedParams(event, z.object({
     key: z.string(),
+  }))
+  const { value } = await zh.useValidatedBody(event, z.object({
     value: z.string(),
   }))
 
